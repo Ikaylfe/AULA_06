b9b7bf15b551a52c992a6092cd745bf13c6e553d
KAYANE CRISTINE AGUIAR DE BRITO <kayane.brito@aluno.senac.rio>
2026-04-14 20:52:25 -0300

    atualiza exemplo 01 aula 06

---
+++ aula06/atividade_aula06_01.py
@@ -0,0 +1,11 @@
+# Atividade média de 10 aluno:
+
+for n in range (10):
+    print(f'\n Aluno {n+1}')
+    nota01=float(input('Insira nota 01 : '))
+    nota02=float(input('Insira nota 02 : '))
+    nota03=float(input('Insira nota 03 : '))
+    nota04=float(input('Insira nota 04 : '))
+    media=(nota01+nota02+nota03+nota04)/4
+    print(f'\n Média do aluno = {media}')
+
