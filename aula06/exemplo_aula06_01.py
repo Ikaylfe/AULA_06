cedbef4d1937e568cd3cb2fdb576d7b11b7564d2
KAYANE CRISTINE AGUIAR DE BRITO <kayane.brito@aluno.senac.rio>
2026-04-14 19:22:13 -0300

    Atualiza repositório

--- aula06/exemplo_aula06_01.py
+++ aula06/exemplo_aula06_01.py
@@ -9,11 +9,18 @@
 
 # Estrutura de Repetição ( For )
 
-for i in range(5):
-    print('Olá Mundo!')
+# for i in range(5): #<range determina valor entre>
+#     print('Olá Mundo!')
+
+# qtde= int (input('Insira até quanto contar?'))
+# for i in range (qtde):
+#     print(i, end= " ") # imprime na mesma linha <end= " ">
 
 
-for i in range (5):
-    print(i)    
-for i in range (5):
-    print(i)    
+# soma=0
+# for u in range (3):
+#     num1= int(input('Informe o número:'))
+#     num2= int(input('Informe o segundo número:'))
+#     soma=num1+num2    
+#     print(f'O total é :{soma}')
+
